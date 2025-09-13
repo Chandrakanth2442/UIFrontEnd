29d0d6ae546ce9fbe159dbf6d864dfa769c429cf
Chandrakanth <chandrakanth2443@gmail.com>
2025-09-13 11:54:18 -0300

    changed code

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         Library library = new Library();
+        System.out.println("New line");
         // Add books
         library.addBook(new Book("The Hobbit", "J.R.R. Tolkien"));
         library.addBook(new Book("1984", "George Orwell"));
